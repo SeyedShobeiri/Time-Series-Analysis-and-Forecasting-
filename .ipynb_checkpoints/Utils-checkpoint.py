f6d9708ad9493c7ad28b18fda68946c02dc7890c
SeyedShobeiri <shobeiri@math.uh.edu>
2021-10-10 16:49:51 -0500

    SARIMAX

--- .ipynb_checkpoints/Utils-checkpoint.py
+++ .ipynb_checkpoints/Utils-checkpoint.py
@@ -1,9 +1,14 @@
 import numpy as np
+from numpy.linalg import LinAlgError
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import probplot, moment
 from statsmodels.tsa.stattools import acf, q_stat, adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import statsmodels.tsa.api as tsa
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.arima_model import ARMA
 
 def plot_correlogram(x, lags=None, title=None):
     lags = min(10, int(len(x)/5)) if lags is None else lags
@@ -34,3 +39,49 @@ def log_transform(x):
     except ValueError as e:
         print("0 element exist")
         
+        
+def optimal_ARMA_lags(train_size,up_bound_p,up_bound_q,y):
+    # train_size (int) 
+    # up_bound_p (int) : highest p to test for AR
+    # up_bound_q (int) : highest q to test for MA
+    # y (series) : time series
+    # freq ('B','D','W','M','A','Q') : frequency of time series
+    # choosing_criteria : options RMSE,BIC,AIC
+    
+    y_true = y[train_size:]
+    results = {}
+    
+    for p in range(up_bound_p):
+        for q in range(up_bound_q):
+            aic,bic = [],[]
+            if p == 0 and q == 0:
+                continue
+            print('ARMA({},{}) is being tested'.format(p,q))
+            
+            convergence_error = stationarity_error = 0
+            y_pred = []
+            
+            for T in range(train_size,len(y)):
+                train_set = y[T-train_size:T]
+                try:
+                    model = ARMA(endog=train_set,order=(p,q)).fit()
+                except LinAlgError:
+                    convergence_error += 1
+                except ValueError:
+                    stationarity_error += 1
+                    
+                forecast, _, _ = model.forecast(steps = 1)
+                y_pred.append(forecast[0])
+                aic.append(model.aic)
+                bic.append(model.bic)
+                
+            result = (pd.DataFrame({'y_true':y_true,'y_pred':y_pred}).replace(np.inf,np.nan).dropna())
+            rmse = np.sqrt(mean_squared_error(y_true = result.y_true,y_pred=result.y_pred))
+            
+            results[(p,q)] = {'Residual Mean Squared Error':rmse,'AIC':np.mean(aic),'BIC':np.mean(bic),'Convergence Error':convergence_error,'Stationarity Error':stationarity_error}
+            
+    arma_results = pd.DataFrame(results).T
+    arma_results.columns = ['RMSE', 'AIC', 'BIC', 'convergence', 'stationarity']
+    arma_results.index.names = ['p', 'q']
+    
+    return arma_results
